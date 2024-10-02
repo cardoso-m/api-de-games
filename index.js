55a1fc95753de63f209ecb9f17d68d4b04361349
Matheus Cardoso <mdonizetecardoso@gmail.com>
2024-10-02 15:01:25 -0300

    Criação da rota para deletar games

--- index.js
+++ index.js
@@ -60,9 +60,23 @@ app.post('/game', (req, res) => {
         price,
         year
     })
-
     res.sendStatus(200)
+})
 
+app.delete('/game/:id', (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400)
+    } else {
+        var id = parseInt(req.params.id)
+        var game = DB.games.findIndex(game => game.id == id)
+
+        if (game == -1) {
+            res.sendStatus(404)
+        } else {
+            DB.games.splice(game, 1)
+            res.sendStatus(200)
+        }
+    }
 })
 
 app.listen(8000, () => {
